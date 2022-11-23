be1a38b86d1eeb7f2bb196eb9232c47e7b17a6cf
Carlos Jr <70163650+zGummy@users.noreply.github.com>
2022-11-23 11:32:46 -0300

    Add files via upload

---
+++ trabalho6/ex01/Ex01-exclui-cliente.php
@@ -0,0 +1,29 @@
+<?php
+require "conexaoMysql.php";
+$pdo = mysqlConnect();
+
+$cpf = "";
+if (isset($_GET["cpf"]))
+  $cpf = $_GET["cpf"];
+
+try {
+
+  $sql = <<<SQL
+  DELETE FROM cliente
+  WHERE cpf = ?
+  LIMIT 1
+  SQL;
+
+  // Neste caso utilize prepared statements para prevenir
+  // ataques do tipo SQL Injection, pois a declaração
+  // SQL contem um parâmetro (cpf) vindo da URL
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute([$cpf]);
+
+  header("location: Ex01-mostra-clientes.php");
+  exit();
+} 
+catch (Exception $e) {  
+  exit('Falha inesperada: ' . $e->getMessage());
+}
+?>
